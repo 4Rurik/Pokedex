1b7a4a09a2e3d90d2f0dc05b7b6bcaa37db236b8
Rodrigo Morales <128872810+4Rurik@users.noreply.github.com>
2023-04-09 17:08:52 -0600

    C4

--- src/context/PokemonProvider.jsx
+++ src/context/PokemonProvider.jsx
@@ -1,12 +1,80 @@
+import { useEffect, useState } from "react"
 import { PokemonContext } from "./PokemonContext"
+import { useForm } from "../hooks/useForm";
 
-export const PokemonProvider = ({ children }) => {
+export const PokemonProvider = ({children}) => {
+
+    const [allPokemons, setAllPokemons] = useState ([]);
+    const [globalPokemons, setGlobalPokemons] = useState ([]);
+    const [offset, setOffset] = useState(0);
+
+    const { valueSearch, onInputChange, onResetForm } = useForm({
+        valueSearch: ''
+    })
+
+    const [loading, setLoading] = useState(true);
+    const [active, setActive] = useState(false);
+
+    const getAllPokemons = async(limit = 50) => {
+        const baseURL = 'https://pokeapi.co/api/v2/';
+
+        const res = await fetch(`${baseURL}pokemon?limit=${limit}&offset=${offset}`);
+        const data = await res.json();
+        
+        const promises = data.results.map(async(pokemon) => {
+            const res = await fetch(pokemon.url);
+            const data = await res.json();
+            return data;
+        });
+
+        const results = await Promise.all(promises);
+
+        setAllPokemons([...allPokemons, ...results]);
+        setLoading(false);
+
+    };
+
+    const getGlobalPokemons = async() => {
+        const baseURL = 'https://pokeapi.co/api/v2/';
+        const res = await fetch(`${baseURL}pokemon?limit=100000&offset=0`);
+        const data = await res.json();
+        const promises = data.results.map(async(pokemon) => {
+            const res = await fetch(pokemon.url);
+            const data = await res.json();
+            return data;
+        });
+
+        const results = await Promise.all(promises);
+
+        setGlobalPokemons(results);
+        setLoading(false);
+    };
+
+    const getPokemonByID = async id => {
+        const baseURL = 'https://pokeapi.co/api/v2/';
+        const res = await fetch(`${baseURL}pokemon/${id}`);
+        const data = await res.json();
+        return data;
+    };
+
+    useEffect(() => {
+        getAllPokemons();
+    }, []);
+
+    useEffect(() => {
+        getGlobalPokemons();
+    }, []);
 
   return (
     <PokemonContext.Provider value={{
-        number: 0
+        valueSearch,
+        onInputChange,
+        onResetForm,
+        allPokemons,
+        globalPokemons,
+        getPokemonByID,
     }}>
-        { children }
+        {children}
     </PokemonContext.Provider>
-  )
-}
+  );
+};
