9c26d00bde2496798b40e7e8501e6055206f4f04
Rodrigo Morales <128872810+4Rurik@users.noreply.github.com>
2023-04-09 18:22:22 -0600

    C9

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { useContext } from 'react'
+import { PokemonContext } from '../context/PokemonContext'
+import { useLocation } from 'react-router-dom';
+import { CardPokemon } from '../components';
 
 export const SearchPage = () => {
+
+  const location = useLocation();
+  const { globalPokemons } = useContext(PokemonContext);
+  const filteredPokemons = globalPokemons.filter(pokemon => pokemon.name.includes(location.state.toLowerCase()));
+
   return (
-    <div>SearchPage</div>
+    <div className="container">
+      <p className="p-search">
+        <span>{filteredPokemons.lenght}</span> Pokémons were found:
+      </p>
+      <div className="card-list-pokemon container">
+        {filteredPokemons.map(pokemon => <CardPokemon pokemon={pokemon} key={pokemon.id} />)}
+      </div>
+    </div>
   )
 }
