e0a0452b74b373ce318873d5f2147c273da4f156
Rodrigo Morales <128872810+4Rurik@users.noreply.github.com>
2023-04-09 23:10:01 -0600

    C13

--- src/components/CardPokemon.jsx
+++ src/components/CardPokemon.jsx
@@ -2,11 +2,22 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export const CardPokemon = ({ pokemon }) => {
+
+	const nameLength = pokemon.name.length;
+
+	let fontSize = '100%';
+
+	if (nameLength >= 10) {
+		fontSize = '80%';
+	} else if (nameLength > 15) {
+		fontSize = '60%';
+	}
+
   return (
     <Link to={`/pokemon/${pokemon.id}`} className='card-pokemon'>
         <div className='card-img'>
 			<div className='card-info'>
-				<div className='name-no'>
+				<div className='name-no' style={{ fontSize }}>
 					<span className='pokemon-id'>No.{pokemon.id}</span>
 					<h3>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h3>
 				</div>
